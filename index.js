f37419d1ba3a8b6ef8be15bd687499f3d58067a5
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-07-02 15:34:44 +0300

    added and removed class list in JS

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ console.log(items);
 
 items.forEach(item => {
     item.addEventListener('mouseover', () => {
-    
+    removeFocus();
+    item.classList.add('selected');
     })
+
+    removeFocus = () => {
+    items.forEach(item => {
+    item.classList.remove('selected');  
+    })
+}
 })
